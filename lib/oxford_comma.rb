d91158c7a74595664a01ad767e9d5e5fd3ab2b2f
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-06 10:22:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length <= 2 
     array.join(" and ")
-  elsif array.length > 2 
+  elsif array.length > 2
+    array.insert(array.length-1, "and")  
     array.join(", ")
-    array.insert(array.length-1, "and")
   end
 end
