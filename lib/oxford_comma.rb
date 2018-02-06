f7ada139babd3b017c1e63a7aaff1ae3bfb675c3
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-06 10:25:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length <= 2 
     array.join(" and ")
   elsif array.length > 2
-    array.join(", ")
-    array.insert(array.length-1, "and")  
+    string = array.join(", ")
+    string.insert(array.length-1, "and")  
   end
 end
